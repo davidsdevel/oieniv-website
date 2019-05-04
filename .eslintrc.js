ab100683e227499bc0157b1775ae39c0a07f691b
Davids-Devel <djgm1206@gmail.com>
2019-05-04 17:53:26 -0400

    cls

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,43 @@ module.exports = {
         "react"
     ],
     "rules": {
+        "arrow-spacing": "error",
+        "no-duplicate-imports": ["error",
+        { 
+            "includeExports": true
+        }],
+        "prefer-destructuring": ["error", {
+            "array": false,
+            "object": true
+        }],
+        "no-useless-constructor": "error",
+        "prefer-template": "error",
+        "no-useless-concat":"error",
+        "no-console":"off",
+        "semi": [
+            "error",
+            "always"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "indent":[
+            "error",
+            "tab"
+        ],
+        "comma-dangle": [
+            "error",
+            "never"
+        ],
+        "eol-last": [
+            "error",
+            "always"
+        ],
+        "comma-spacing": ["error",
+        {
+            "before": false,
+            "after": true
+        }]
     }
 };
