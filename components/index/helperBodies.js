c1d68a0564cf7ac25accf1209c2ea9777927365b
David-ImpulseWD <djgm1206@gmail.com>
2019-05-07 15:50:50 -0400

    Added HelperBodies component to index page

--- components/index/helperBodies.js
+++ components/index/helperBodies.js
@@ -1,9 +1,72 @@
 import React from "react";
 import HelperBody from "./helperBody";
 
+const data = [
+	{
+		name: "Anjeiv",
+		content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		logoURL: "/static/images/test-logo.png"
+	},
+	{
+		name: "Andeiv",
+		content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		logoURL: "/static/images/test-logo.png"
+	},
+	{
+		name: "Copaso",
+		content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		logoURL: "/static/images/test-logo.png"
+	},
+	{
+		name: "Pronani",
+		content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		logoURL: "/static/images/test-logo.png"
+	},
+	{
+		name: "Anjeiv",
+		content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		logoURL: "/static/images/test-logo.png"
+	},
+	{
+		name: "Andeiv",
+		content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		logoURL: "/static/images/test-logo.png"
+	},
+	{
+		name: "Copaso",
+		content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		logoURL: "/static/images/test-logo.png"
+	},
+	{
+		name: "Pronani",
+		content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		logoURL: "/static/images/test-logo.png"
+	}
+];
+
+const childs = data.map(({name, content, logoURL}, i) => ( <HelperBody key={`Organos Auxiliar-${i}`} name={name} content={content} logoURL={logoURL}/>));
 const HelperBodies = () => (
-	<div>
-		<HelperBody />
+	<div id="helper-bodies-container">
+		<h3 className="sub-titles">Organos Auxiliares de OIENIV</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		{childs}
+		<style jsx="true">{`
+			#helper-bodies-container {
+				with: 80%;
+				padding: 50px 10%;
+			}
+			#helper-bodies-container .helper-body {
+				width: 20%;
+			}
+			#helper-bodies-container .helper-body img {
+				width: 100%;
+			}
+		`}</style>
 	</div>
 );
 
--- components/index/helperBody.js
+++ components/index/helperBody.js
@@ -1,7 +1,18 @@
 import React from "react";
+import {string} from "prop-types";
 
-const HelperBody = () => (
-	<div></div>
+const HelperBody = ({logoURL, name, content}) => (
+	<div className="helper-body">
+		<h4>{name}</h4>
+		<img src={logoURL} alt={`Organos Auxiliares - ${name} - OIENIV`}/>
+		<p>{content}</p>
+	</div>
 );
+
+HelperBody.propTypes = {
+	logoURL: string,
+	name: string,
+	content: string
+};
 
 export default HelperBody;
