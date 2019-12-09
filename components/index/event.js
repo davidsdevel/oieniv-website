c9a724a129b36162b1ae93a1d0e4e7f33a311b0d
davidsdevel <davidsdevel@gmail.com>
2019-12-09 19:54:25 -0400

    Created Events Slider

--- components/index/event.js
+++ components/index/event.js
@@ -2,7 +2,116 @@ import React from "react";
 import {object} from "prop-types";
 
 const Event = ({data}) => {
-	return <div></div>
+	const date = new Date(data.date + " " + data.time);
+	const time = date.getHours();
+	var minutes = date.getMinutes();
+
+	if (minutes < 10)
+		minutes = "0" + minutes;
+	else if (minutes === 0)
+		minutes = "";
+
+	let hour;
+	if (time < 12)
+		hour = `${time}:${minutes}AM`;
+	else if (time === 12)
+		hour = `${time}:${minutes}M`;
+	else if (time > 12)
+		hour = `${time - 12}:${minutes}PM`;
+	else if (time === 0)
+		hour = "12AM";
+
+	let month;
+	switch(date.getMonth()) {
+		case 0:
+			month = "Enero";
+			break;
+		case 1:
+			month = "Febrero";
+			break;
+		case 2:
+			month = "Marzo";
+			break;
+		case 3:
+			month = "Abril";
+			break;
+		case 4:
+			month = "Mayo";
+			break;
+		case 5:
+			month = "Junio";
+			break;
+		case 6:
+			month = "Julio";
+			break;
+		case 7:
+			month = "Agosto";
+			break;
+		case 8:
+			month = "Septiembre";
+			break;
+		case 9:
+			month = "Octubre";
+			break;
+		case 10:
+			month = "Noviembre";
+			break;
+		case 11:
+			month = "Diciembre";
+			break;
+		default: break;
+	}
+
+
+
+	return <div style={{backgroundImage: data.image ? `url(${data.image})` : ""}} className="template">
+		<div id="template-shadow">
+			<h3>{data.name}</h3>
+			<p>{data.description}</p>
+			<span id="date">El {date.getDate()} de {month} a las {hour}</span>
+			<span id="church">En la Iglesia <b>{data.church}</b></span>
+			<span id="location">Zona <b>{data.location}</b></span>
+		</div>
+		<style jsx>{`
+			.template {
+				position: absolute;
+				width: 100%;
+				height: 100%;
+				color: white;
+
+				background-size: cover;
+				background-position: center;
+			}
+			.template h3 {
+				text-align: center;
+				margin-top: 80px;
+			}
+			.template span {
+				display: block;
+				position: absolute;
+			}
+			#template-shadow {
+				background: rgba(0,0,0,.5);
+				position: absolute;
+				width: 100%;
+				height: 100%;
+			}
+			#date {
+				right: 5%;
+				bottom: 15%;
+			}
+			#location {
+				bottom: 15%;
+				left: 5%;
+			}
+			#church {
+				width: 100%;
+				text-align: center;
+				left: 0;
+				bottom: 5%;
+			}
+		`}</style>
+	</div>
 }
 
 Event.propTypes = {
--- components/index/eventsSlider.js
+++ components/index/eventsSlider.js
@@ -7,16 +7,68 @@ class Slider extends Component {
 		super(props);
 		this.state = {
 			...props,
-			total: props.data.lenght
+			total: props.data.length,
+			active: 0,
+			test: [1,2,3,4,5,6]
 		};
+
+		this.timeout;
+
+		this.componentDidMount = this.componentDidMount.bind(this);
+	}
+	componentDidMount() {
+		/*setTimeout(() => {
+			if (this.state.total - 1 === this.state.active) {
+				this.setState({
+					active: 0
+				});
+			} 
+		}, 6000);*/
+		setInterval(() => {
+			var a = Object.assign([], this.state.test);
+
+			a.push(a[0]);
+			a.shift();
+
+			this.setState({
+				test: a
+			});
+		}, 1000);
 	}
 	render() {
 		const {data} = this.state;
 
-		return <div>
-			{data.map((e, i) => <div key={e.name + i}>
+		return <div id="slider-container">
+			<button id="prev">Ant.</button>
+			{data.map((e, i) => <div key={e.name + i} className={this.state.active === i ? "active" : ""}>
 				<Template data={e} />
 			</div>)}
+			<button id="next">Sig.</button>
+			<style jsx>{`
+				#slider-container {
+					background: red;
+					width: 100%;
+					height: 500px;
+					position: relative;
+				}
+				button {
+					position: absolute;
+					width: 50px;
+					height: 50px;
+					border-radius: 50%;
+					border: none;
+					color: #3c374e;
+					background: white;
+					top: calc(50% - 25px);
+				}
+				button#next {
+					right: 5%;
+				}
+				button#prev {
+					z-index: 1;
+					left: 5%;
+				}
+			`}</style>
 		</div>;
 	}
 }
